2966e9f493dbf78cbc893e476f3cbbf77900f838
Rahil Taujale <rtaujale@uga.edu>
2025-04-30 10:37:22 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # dataset
-wget https://www.dropbox.com/sh/uxbsymtziktlu0r/AACkVr6IhR87OCp8z_UsTchFa?dl=0 -O Datasets.zip
+wget https://zenodo.org/records/15310844/files/Datasets.zip?download=1 -O Datasets.zip
 unzip Datasets.zip -d Datasets/
 rm Datasets.zip
 
 # pretrained model
-wget https://www.dropbox.com/sh/44ftj7zfcpaaxce/AACwcJ_1mqwV3xqQwajNi89Ga?dl=0 -O PretrainedModels.zip
+wget https://zenodo.org/records/15310844/files/PretrainedModels.zip?download=1 -O PretrainedModels.zip
 unzip PretrainedModels.zip -d PretrainedModels/
 rm PretrainedModels.zip
 
 # example outputs
-wget https://www.dropbox.com/sh/5lr1ciigpigln72/AACpl447nLlrXqgKpyRuyddTa?dl=0 -O ExampleOutputs.zip
+wget https://zenodo.org/records/15310844/files/ExampleOutputs.zip?download=1 -O ExampleOutputs.zip
 unzip ExampleOutputs.zip -d ExampleOutputs/
 rm ExampleOutputs.zip
